34e241b58320834da930245c903f7f6cac18874a
erikbackman <erikbackman@users.noreply.github.com>
2024-02-21 13:07:58 +0100

    Forgot to commit this

--- src/c.zig
+++ src/c.zig
@@ -3,4 +3,6 @@ pub usingnamespace @cImport({
     @cInclude("X11/XF86keysym.h");
     @cInclude("X11/keysym.h");
     @cInclude("X11/XKBlib.h");
+    @cInclude("X11/Xatom.h");
+    @cInclude("X11/Xutil.h");
 });
